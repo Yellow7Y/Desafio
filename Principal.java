4e4adbdfb8df7e59b7adb81b36234929792a5ff7
Yellow7Y <102256640+Yellow7Y@users.noreply.github.com>
2022-06-08 14:03:37 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,4 +1,4 @@
-package DESAFIO;
+package Desafio;
 
 /*
 Ao analisar as classes do projeto, percebemos que a classe Pessoa serve somente como uma classe pai para Cliente e Fornecedor. Não gostaríamos que a classe
@@ -34,20 +34,40 @@ public class Principal {
         Cliente telecom = new Cliente();
         telecom.setNome("FoneNet Telecomunicações");
         // instanciando contas a pagar
-        ContaPagar contaPagar1 = new ContaPagar();
+        ContaPagar contaPagar1 = new ContaPagar() {
+            @Override
+            public void receber() {
+
+            }
+        };
         contaPagar1.setDescricao("Aluguel da matriz");
         contaPagar1.setValor(1230d);
         contaPagar1.setDataVencimento("10/05/2012");
         contaPagar1.setFornecedor(imobiliaria);
-        ContaPagar contaPagar2 = new ContaPagar(mercado, "Compras do mês", 390d, "19/05/2012");
+        ContaPagar contaPagar2 = new ContaPagar(mercado, "Compras do mês", 390d, "19/05/2012") {
+            @Override
+            public void receber() {
+
+            }
+        };
         // instanciando contas a receber
-        ContaReceber contaReceber1 = new ContaReceber();
+        ContaReceber contaReceber1 = new ContaReceber() {
+            @Override
+            public void pagar() {
+
+            }
+        };
         contaReceber1.setDescricao("Desenvolvimento de projeto de logística em Java");
         contaReceber1.setValor(89500d);
         contaReceber1.setDataVencimento("23/05/2012");
         contaReceber1.setCliente(atacadista);
         ContaReceber contaReceber2 = new ContaReceber(telecom, "Manutenção em sistema de conta online",
-                53200d, "13/05/2012");
+                53200d, "13/05/2012") {
+            @Override
+            public void pagar() {
+
+            }
+        };
         // exibe listagem de todas as contas com detalhamento
         RelatorioContas relatorio = new RelatorioContas();
         Conta[] contas = new Conta[]{contaPagar1, contaPagar2, contaReceber1, contaReceber2};
